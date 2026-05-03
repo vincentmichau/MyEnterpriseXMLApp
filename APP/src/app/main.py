59dc168ce470f414e95ae68687add9b4e0b87b14
Vincent MICHAU <vincent.michau@gmail.com>
2026-05-03 20:27:03 +0200

    Refactor imports and enhance MainWindow setup
    bug fix

--- APP/src/app/main.py
+++ APP/src/app/main.py
@@ -1,20 +1,25 @@
-
-from PySide6.QtWidgets import QApplication, QMainWindow, QAction
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtGui import QAction
 import sys
+
 from core.i18n import I18N
 from ui.xml_tree import XMLTree
 from version import APP_NAME
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.i18n = I18N('fr')
         self.setWindowTitle(APP_NAME)
         self.resize(1000, 700)
+
         self.xml_view = XMLTree(self.i18n)
         self.setCentralWidget(self.xml_view)
 
         bar = self.menuBar()
+
         self.file_menu = bar.addMenu(self.i18n.t('file'))
         quit_action = QAction(self.i18n.t('quit'), self)
         quit_action.triggered.connect(self.close)
